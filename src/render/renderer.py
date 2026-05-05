3b9021470acd9b81a218feb3af4b9e46c94dbc3b
Fabio <fabiopyrodias@gmail.com>
2026-05-05 20:50:54 +0100

    More progress on Renderer

--- src/render/renderer.py
+++ src/render/renderer.py
@@ -1,5 +1,6 @@
 import pygame
 import time
+from typing import Any
 from src.manager import Manager
 from src.map.node import Node
 
@@ -31,21 +32,27 @@ COLORS = {
 }
 
 class NodeVisual():
-    def __init__(self, node: Node) -> None:
-        if node.color != "rainbow":
-            node.color = COLORS[node.color]
+    def __init__(self, coords: tuple[Any, Any], color: str) -> None:
+        self.coords = coords
+        if color != "rainbow":
+            self.color = COLORS[color]
         else:
             pass
 
-    def draw(self, screen: pygame.Surface) -> None:
-        pygame.draw.circle(self.screen, COLORS[node.color], 
-                           (self.width / 2 * node.coords[0] / self.middle_point[0] + self.width / 4,
-                           self.height / 2 * node.coords[1]),
+    def draw(self) -> None:
+        pygame.draw.circle(self.screen, COLORS[self.color], 
+                           (self.width / 2 * self.coords[0] / self.middle_point[0] + self.width / 4,
+                           self.height / 2 * self.coords[1]),
                            self.scale / 3)
 
 
+class ConnectionVisual():
+    def __init__(self) -> None:
+        pass
+
+
 class DroneVisual():
-    def __init__(self): -> None:
+    def __init__(self) -> None:
         pass
 
 
@@ -53,24 +60,15 @@ class Renderer():
     def __init__(self, manager: Manager):
         self.manager = manager
         pygame.init()
-        self.width = 2000
-        self.height = 1000
-        self.draw_width = self.width - 400
-        self.draw_height = self.height - 200
+
+        self.set_window_size()
+        self.create_visual_nodes()
+
         self.screen = pygame.display.set_mode((self.width, self.height))
         self.clock = pygame.time.Clock()
-        self.scale = 100
-
-        coords_x = sorted(self.manager.graph.nodes.values(), key=lambda node: node.coords[0])
-        coords_y = sorted(self.manager.graph.nodes.values(), key=lambda node: node.coords[1])
-        self.coords_x = [coords_x[0].coords[0], coords_x[-1].coords[0]]
-        self.coords_y = [coords_y[0].coords[1], coords_y[-1].coords[1]]
-        self.middle_point = ((self.coords_x[0] + self.coords_x[1]) / 2,
-                             (self.coords_y[0] + self.coords_y[1]) / 2)
-
 
         # Passos deste init:
-        # 1. Preparar janela para a impressao do mapa.
+        # 1. Preparar janela para a impressao do mapa.                  DONE
         # 2. Criar representacoes visuais dos Nodes (NodeVisual)
         #   2.1. Definir cor
         #   2.2. Definir coordenadas adaptadas a janela.
@@ -82,46 +80,100 @@ class Renderer():
         # 5. Percorrer lista de turnos e cada evento
         #   5.1. Recriar eventos com animacao (X frames / Y segundos para cada turno simulado)
 
-    def set_coords(self, node_coords: tuple[int, int]) -> tuple[int, int]:
-        coord_x = 0
-        coord_y = 0
+    def set_window_size(self) -> None:
+        self.scale = 100
 
-        mid_point_x = self.draw_width / 2
-        mid_point_y = self.draw_height / 2
-        initial_pos_x = (self.width - self.draw_width) / 2
-        initial_pos_y = (self.height - self.draw_height) / 2
+        min_width = 800
+        max_width = 1600
+        min_height = 700
+        max_height = 900
 
-        """ if self.middle_point[0] == 0:
-            coord_x = mid_point_x * node_coords[0] / self.middle_point[0]
-        else:
-            coord_x = initial_pos_x + (mid_point_x * node_coords[0] / self.middle_point[0])
+        self.width = min_width
+        self.height = min_height
 
-        if self.middle_point[1] == 0:
-            if self.coords_y[-1].coords[1] == 0:
-                coord_y = self.height / 2
-            else:
-                coord_y = initial_pos_y + (((node_coords[1] - self.coords_y[0].coords[1]) * ((self.draw_height) - initial_pos_y)) / (self.coords_y[-1].coords[0] - self.coords_y[0].coords[0]))
-        else:
-            coord_y = mid_point_y * node_coords[1] / self.middle_point[1]
+        self.draw_width_margin = 200
+        self.draw_height_margin = 100
+        self.draw_width = self.width - self.draw_width_margin
+        self.draw_height = self.height - self.draw_height_margin
 
-        print(f"node_coords: {node_coords} -> {(coord_x, coord_y)}")
-        return (coord_x, coord_y) """
+        sorted_coords_x = sorted(self.manager.graph.nodes.values(),
+                                 key=lambda node: node.coords[0])
+        sorted_coords_y = sorted(self.manager.graph.nodes.values(),
+                                 key=lambda node: node.coords[1])
 
-    def draw_nodes(self):
-        print(self.middle_point)
+        self.coords_x = [sorted_coords_x[0].coords[0],
+                         sorted_coords_x[-1].coords[0]]
+        self.coords_y = [sorted_coords_y[0].coords[1],
+                         sorted_coords_y[-1].coords[1]]
+
+        dimensions_x = (abs(self.coords_x[0]) +
+                        abs(self.coords_x[1])) * self.scale
+        dimensions_y = (abs(self.coords_y[0]) +
+                        abs(self.coords_y[1])) * self.scale
+
+        if dimensions_x > min_width:
+            self.width = dimensions_x
+            self.draw_width = self.width - self.draw_width_margin
+        if dimensions_y > min_height:
+            self.height = dimensions_y
+            self.draw_height = self.height - self.draw_height_margin
+
+        scale_x = self.scale
+        scale_y = self.scale
+
+        if self.width > max_width:
+            self.width = max_width
+            self.draw_width = max_width - self.draw_width_margin
+            scale_x = (max_width /
+                       (abs(self.coords_x[0]) + abs(self.coords_x[1])))
+        if self.height > max_height:
+            self.height = max_height
+            self.draw_height = max_height - self.draw_height_margin
+            scale_y = (max_height /
+                       (abs(self.coords_y[0]) + abs(self.coords_y[1])))
+
+        if abs(self.coords_x[0]) + abs(self.coords_x[1]) != 0:
+            scale_x = (self.draw_width /
+                       (abs(self.coords_x[0]) + abs(self.coords_x[1])))
+        if abs(self.coords_y[0]) + abs(self.coords_y[1]) != 0:
+            scale_y = (self.draw_height /
+                       (abs(self.coords_y[0]) + abs(self.coords_y[1])))
+
+        self.scale = scale_x if scale_x <= scale_y else scale_y
+        self.scale = int(self.scale)
+
+        self.middle_point_x = (self.coords_x[1] - self.coords_x[0]) / 2
+        self.middle_point_y = (self.coords_y[1] - self.coords_y[0]) / 2
+
+    def create_visual_nodes(self) -> None:
+        self.visual_nodes = []
+
+        map_range_x = self.coords_x[1] - self.coords_x[0]
+        map_range_y = self.coords_y[1] - self.coords_y[0]
+
         for node in self.manager.graph.nodes.values():
-            if node.color == "rainbow":
-                pass
+            x = 0
+            y = 0
+
+            if map_range_x == 0:
+                x = self.width / 2
             else:
-                pygame.draw.circle(self.screen, COLORS[node.color], 
-                                   self.set_coords(node.coords),
-                                   self.scale / 8)
+                x = (((node.coords[0] - self.coords_x[0]) * self.draw_width) /
+                     map_range_x) + self.draw_width_margin / 2
+
+            if map_range_y == 0:
+                y = self.height / 2
+            else:
+                y = (((node.coords[1] - self.coords_y[0]) * self.draw_height) /
+                     map_range_y) + self.draw_height_margin / 2
+
+            self.visual_nodes.append((x, y), node.color)
 
     def run(self) -> None:
         for turn in self.manager.turns:
             for event in turn:
                 #self.screen.fill()
-                self.draw_nodes()
+                #self.draw_nodes()
                 pygame.display.flip()
                 time.sleep(2)
         #pygame.quit()
